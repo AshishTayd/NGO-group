c7277974372edc247a920005dc6ec6ac4d586e5c
Aarya Walunjkar <aaryawalunjkar01@gtasterix.com>
2024-09-25 14:04:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Open the slider
+document.getElementById("openSliderBtn").onclick = function() {
+    document.getElementById("accountSlider").classList.add("slider-open");
+}
+
+// Close the slider
+document.getElementById("closeSliderBtn").onclick = function() {
+    document.getElementById("accountSlider").classList.remove("slider-open");
+}
